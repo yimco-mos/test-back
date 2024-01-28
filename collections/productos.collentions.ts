1793f1587300ad36a4db511fdb60d15fe8b0c5b5
yimco <yimco@yimco.com>
2024-01-28 11:36:09 -0500

    test back last state

---
+++ collections/productos.collentions.ts
@@ -0,0 +1,25 @@
+// collections/productos.collection.ts
+
+export const ProductosCollection = [
+  {
+    id: 1,
+    nombre: 'Producto 1',
+    precio: 10.99,
+    descripcion: 'Este es el producto 1',
+    stock: 100,
+  },
+  {
+    id: 2,
+    nombre: 'Producto 2',
+    precio: 15.49,
+    descripcion: 'Este es el producto 2',
+    stock: 50,
+  },
+  {
+    id: 3,
+    nombre: 'Producto 3',
+    precio: 20.99,
+    descripcion: 'Este es el producto 3',
+    stock: 200,
+  },
+];
---
+++ collections/tienda.collections.ts
@@ -0,0 +1,22 @@
+// collections/tiendas.collection.ts
+
+export const TiendasCollection = [
+  {
+    id: 1,
+    nombre: 'Tienda 1',
+    direccion: 'Calle Principal 123',
+    ciudad: 'Ciudad 1',
+  },
+  {
+    id: 2,
+    nombre: 'Tienda 2',
+    direccion: 'Avenida Secundaria 456',
+    ciudad: 'Ciudad 2',
+  },
+  {
+    id: 3,
+    nombre: 'Tienda 3',
+    direccion: 'Paseo Central 789',
+    ciudad: 'Ciudad 3',
+  },
+];
